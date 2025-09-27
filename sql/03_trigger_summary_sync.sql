8837d86b189dad1090dac0c8133a90266fa6ef26
revrodge <derekrogers007@gmail.com>
2025-09-27 15:05:28 -0600

    added a drop for existing instances and added some comments

--- sql/03_trigger_summary_sync.sql
+++ sql/03_trigger_summary_sync.sql
@@ -7,12 +7,18 @@
 --   are inserted into report_rental_detail
 -- ================================================================
 
+
+-- drops old triggers... just in case
+DROP TRIGGER IF EXISTS trg_detail_after_insert ON report_rental_detail;
+DROP FUNCTION IF EXISTS trg_detail_after_insert();
+
 -- trigger function: roll up the inserted detail row
 CREATE OR REPLACE FUNCTION trg_detail_after_insert()
 RETURNS trigger
 LANGUAGE plpgsql
 AS $$
 BEGIN
+    -- attempts to insert new row in summary table for a given pk combo
   INSERT INTO report_category_monthly (
       report_month, store_id, category_id, store_name, category_name,
       rentals_count, total_revenue, total_days_rented, late_rentals
@@ -28,7 +34,7 @@ BEGIN
       COALESCE(NEW.days_rented, 0),
       CASE WHEN NEW.on_time_flag = 'N' THEN 1 ELSE 0 END
   )
-  -- handles existing pk combinations by udating data in those rows
+  -- handles existing pk combinations by udating data in those rows instead of adding new rows
   ON CONFLICT (report_month, store_id, category_id) DO UPDATE
   --this is the row we're adding
   SET rentals_count     = report_category_monthly.rentals_count + 1, 
